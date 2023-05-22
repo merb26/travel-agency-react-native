74a83dd2a06f51ad00bbb13e491245750d8b523b
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-05-22 16:09:07 -0600

    change margin bottom on ListItems component

--- src/components/list-items/styles.js
+++ src/components/list-items/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    marginBottom: 10,
+    marginBottom: 73,
     marginHorizontal: 10,
     paddingVertical: 10,
   },
