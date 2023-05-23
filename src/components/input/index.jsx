8f378faa90a2b8a7f9e54267476330c7bad646e5
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-05-23 02:21:33 -0600

    installation redux and react-redux, add attributes on Input, add hooks and login on Flights, create redux architecture

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -2,10 +2,15 @@ import { TextInput, View } from "react-native";
 
 import { styles } from "./styles";
 
-const Input = ({ placeHolderText, style }) => {
+const Input = ({ placeHolderText, style, value, setValue }) => {
   return (
     <View style={[styles.container, style]}>
-      <TextInput style={styles.inputText} placeholder={placeHolderText} />
+      <TextInput
+        style={styles.inputText}
+        placeholder={placeHolderText}
+        value={value}
+        onChangeText={(text) => setValue(text)}
+      />
     </View>
   );
 };
--- src/screens/flights/index.jsx
+++ src/screens/flights/index.jsx
@@ -8,21 +8,36 @@ import { flights } from "../../database";
 
 const Flights = () => {
   const [dataVuelos, setDataVuelos] = useState(flights);
+  const [listFlights, setListFlights] = useState(flights)
+  const [valueOrigin, setValueOrigin] = useState("");
+
+  const handlePressSearch = () => {
+    const search = dataVuelos.filter((item) => {
+      return item.origen === valueOrigin;
+    });
+
+    setListFlights(search)
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.boxInputs}>
-        <Input style={styles.input} placeHolderText="Ingrese el origen" />
+        <Input
+          style={styles.input}
+          placeHolderText="Ingrese el origen"
+          value={valueOrigin}
+          setValue={setValueOrigin}
+        />
         <Input style={styles.input} placeHolderText="Ingrese el destino" />
       </View>
       <View style={styles.boxButton}>
         <ButtonCustom
           style={styles.buttonCustom}
-          onPressButton={() => console.warn("hiciste clik")}
+          onPressButton={handlePressSearch}
           title="Buscar"
         />
       </View>
-      <ListItems data={dataVuelos} Item={ItemFlights} />
+      <ListItems data={listFlights} Item={ItemFlights} />
     </View>
   );
 };
