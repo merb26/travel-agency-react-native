fc6a186895c9d0b00b48462ada4fcbc8c5800906
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-05-23 11:52:05 -0600

    changed of data on ItemFlights

--- src/components/item-flights/index.jsx
+++ src/components/item-flights/index.jsx
@@ -5,11 +5,12 @@ import styles from "./styles";
 const ItemFlights = ({ item }) => (
   <View style={styles.boxCard}>
     <View style={styles.boxOriginDestination}>
-      <Text style={styles.origen}>Origen: {item.origen}</Text>
-      <Text style={styles.destino}>Destino: {item.destino}</Text>
+      <Text style={styles.origen}>Origen: {item.origen.pais}</Text>
+      <Text style={styles.destino}>Destino: {item.destino.pais}</Text>
     </View>
-    <Text style={styles.fecha}>Fecha: {item.fecha}</Text>
-    <Text style={styles.horario}>Horario: {item.horario} hrs</Text>
+    <Text style={styles.fecha}>Fecha: {item.fecha.salida}</Text>
+    <Text style={styles.horario}>Horario salida: {item.hora_salida} hrs</Text>
+    <Text style={styles.horario}>Horario llegada: {item.hora_llegada} hrs</Text>
     <Text style={styles.tipo}>Tipo: {item.tipo}</Text>
   </View>
 );
